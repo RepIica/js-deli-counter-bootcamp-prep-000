17342c507ff37011f2d51d0400f9dd2a9f3c7635
Sammy Akharaz <ketrixz@gmail.com>
2018-05-03 15:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function takeANumber(line) {
  line.push(line.length + 1)
  return `You are number ${line.length + 1}.`
 }
+
 function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!"
